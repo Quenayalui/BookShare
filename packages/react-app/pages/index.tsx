7abb62eacddf3b88e06997d0b0472516134f1ead
sebi900 <lange.sebastian95@gmx.de>
2023-06-28 20:31:35 +0200

    first commit

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <div>
       <div className="h1">There you go... a canvas for your next Celo project!</div>
+      <input type= "file" name="Upload" ></input>
+   
     </div>
   )
 }
