5a566a6e75ea7de68936700ff5999235ad522df8
nort4n <trikkster@gmail.com>
2023-06-29 00:01:34 +0200

    contract mockup

--- packages/hardhat/contracts/DonateContract.sol
+++ packages/hardhat/contracts/DonateContract.sol
@@ -3,17 +3,22 @@ pragma solidity ^0.8.4;
 
 contract DonateContract {
   uint totalDonations; // the amount of donations
-  address payable owner; // contract creator's address
+  address owner; // contract creator's address
 
   // Other contract properties and variables
 
   constructor() {
-    owner = payable(msg.sender); // setting the contract creator
+    owner = msg.sender; // setting the contract creator
   }
 
-function donate(uint amount) public payable {
-  require(msg.value == amount, "Incorrect donation amount");
-  totalDonations += amount;
-  // Other logic for handling the donation
-}
+  function donate(uint amount) public payable {
+    require(msg.value == amount, "Incorrect donation amount");
+    totalDonations += amount;
+    // Other logic for handling the donation
+    // returning excess amount or putting it towards other publications 
+  }
+
+  // function transfer to transfer the sufficient amount for financing the storage on bundler 
+  
+
 }
