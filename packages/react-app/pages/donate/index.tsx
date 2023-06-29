f747c759dc9fc4d2358a7be94a5cac1471fb923d
ehidne-mora <mora@slime.garden>
2023-06-29 08:21:54 +0200

    Small CSS changes.

--- packages/react-app/pages/donate/index.tsx
+++ packages/react-app/pages/donate/index.tsx
@@ -3,17 +3,22 @@ import DonateContractFactory from "../../abis/DonateContractFactory.json";
 import { parseEther } from "viem";
 
 export default function DonatePage() {
-    const { data, isLoading, isSuccess, write } = useContractWrite({
-        address: DonateContractFactory.address,
-        abi: DonateContractFactory.abi,
-        functionName: "createNewDonateContract",
-        args: [parseEther("0.1")],
-    });
-    return (
-        <div>
-            <button onClick={() => write()}>Donate</button>
-            {isLoading && <div>Check Wallet</div>}
-            {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
-        </div>
-    );
+	const { data, isLoading, isSuccess, write } = useContractWrite({
+		address: DonateContractFactory.address,
+		abi: DonateContractFactory.abi,
+		functionName: "createNewDonateContract",
+		args: [parseEther("0.1")],
+	});
+	return (
+		<div>
+			<button
+				className="bg-black px-3 py-2 rounded-s text-white"
+				onClick={() => write()}
+			>
+				Donate
+			</button>
+			{isLoading && <div>Check Wallet</div>}
+			{isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+		</div>
+	);
 }
